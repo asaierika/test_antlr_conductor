3c913cf5886089a3c155bce14be479df2abf8d39
asaierika <anyaasai@gmail.com>
2025-03-29 18:50:12 +0800

    Test deployment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { initialise } from "conductor/dist/conductor/runner/util/";
 import { SimpleLangEvaluator } from "./SimpleLangEvaluator";
 
+// test
 console.log("Custom evaluator loaded successfully!");
 
 const { runnerPlugin, conduit } = initialise(SimpleLangEvaluator);
